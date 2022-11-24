4700c04c0c6950b664fb24187a53639b2431d8ae
Kwon Valerii <wellko24@gmail.com>
2022-11-24 17:58:43 +0600

    Created CountriesList.tsx List of all countries and styles for them CountriesList.css

---
+++ src/Components/CountriesList/CountriesList.tsx
@@ -0,0 +1,62 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import axios from 'axios';
+import {Country, CountryInfoResponse} from "../../types";
+import CountryInfo from "../CountryInfo/CountryInfo";
+import './CountriesList.css';
+
+const CountriesList = () => {
+    const url = 'https://restcountries.com/v2/all?fields=alpha3Code,name';
+    const [countryList, setCountryList] = useState<CountryInfoResponse[]>([]);
+
+    useEffect(() => {
+        fetchData().catch(console.error);
+    }, []);
+
+    const fetchData = useCallback(async () => {
+        const postsResponse = await axios.get<Country[]>(url);
+        const promises = postsResponse.data.map(async item => {
+            const countryUrl = 'https://restcountries.com/v2/alpha/' + item.alpha3Code + '?fields=name,region,population,flag,borders';
+            const userResponse = await axios.get<CountryInfoResponse>(countryUrl);
+            return {
+                name: userResponse.data.name,
+                region: userResponse.data.region,
+                population: userResponse.data.population,
+                flag: userResponse.data.flag,
+                borders: userResponse.data.borders,
+            };
+        });
+        const newCountryList = await Promise.all(promises);
+        setCountryList(newCountryList);
+    }, []);
+
+    const [countryData, setCountryData] = useState<CountryInfoResponse>({
+        name: '',
+        region: '',
+        population: '',
+        flag: '',
+        borders: [],
+    })
+
+    const countryClick = (item: CountryInfoResponse) => {
+        setCountryData(item);
+    }
+
+
+    return (
+            <div className='GridContainer'>
+                <>
+                    <ul className='countryList'>
+                        {countryList.map(item => (
+                            <li className='list' key={Math.random()} onClick={() => countryClick(item)}>
+                                {item.name}
+                            </li>))}
+                    </ul>
+                </>
+                <>
+                    <CountryInfo country={countryData}/>
+                </>
+            </div>
+    );
+};
+
+export default CountriesList;
