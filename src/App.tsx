c5d65ff3b0f3cb63843ed6c1999a41a950fa4114
Kwon Valerii <wellko24@gmail.com>
2022-11-24 18:02:32 +0600

    added all components in container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import CountriesList from "./Components/CountriesList/CountriesList";
 
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    return (
+        <div className="App">
+            <CountriesList/>
+        </div>
+    );
 }
 
 export default App;
